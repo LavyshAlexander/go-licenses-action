e0865d06fe7aeb19db08127ead5cfb6f34413a22
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-09-12 00:43:42 +0300

    Change path param of go-license.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-output=$(go-licenses check ./... 2>&1 --allowed_licenses="$1" --ignore="$2")
+output=$(go-licenses check . 2>&1 --allowed_licenses="$1" --ignore="$2")
 
 if [ -z "$output" ]; then
     echo "Go Licenses: Check Success"
