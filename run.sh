7aa3bb437b8204f447419b77904745660367a354
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-09-12 00:03:54 +0300

    Added first version of action and run.sh script.

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+output=$(go-licenses check ./... --allowed_licenses="$1" --ignore="$2")
+
+if [ -z "$output" ]; then
+    echo "Go Licenses: Check Success"
+    exit 0
+else
+    echo "$output" "Go Licenses: Check Failed"
+    exit 1
+fi
