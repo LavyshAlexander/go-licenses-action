7be432a2497731cfbc58b796511f37c59de8434e
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-09-12 00:41:28 +0300

    Send stderr output of go-licenses to stdout.

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-output=$(go-licenses check ./... --allowed_licenses="$1" --ignore="$2")
+output=$(go-licenses check ./... 2>&1 --allowed_licenses="$1" --ignore="$2")
 
 if [ -z "$output" ]; then
     echo "Go Licenses: Check Success"
     exit 0
 else
-    echo "$output" "Go Licenses: Check Failed"
+    echo "$output"
+	echo "Go Licenses: Check Failed"
     exit 1
 fi
